f02dc88231694838103febdafdcbc1e4c8532d57
Sheonna Harris <svonharris@gmail.com>
2025-08-16 23:21:29 -0400

    Added area chart from Apex Charts.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,11 @@
 import "./Card.css";
 import { motion, AnimatePresence } from "motion/react"; // motion.dev import
 import { useState } from "react";
+import Chart from "react-apexcharts";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { UilTimes } from "@iconscout/react-unicons";
+import { s } from "framer-motion/client";
 
 const Card = (props) => {
   const [expanded, setExpanded] = useState(false);
@@ -65,6 +67,55 @@ function CompactCard({ params, setExpanded }) {
 
 // Expanded Card
 function ExpandedCard({ params, setExpanded }) {
+  const data = {
+    options: {
+      chart: {
+        type: "area",
+        height: "auto",
+      },
+      dropShadow: {
+        enabled: false,
+        enabledOnSeries: undefined,
+        top: 0,
+        left: 0,
+        blur: 3,
+        color: "#000",
+        opacity: 0.35,
+      },
+      fill: {
+        colors: ["#fff"],
+        type: "gradient",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+        colors: ["#fff"],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+      grid: {
+        show: true,
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      },
+    },
+  };
+
   return (
     <motion.div
       layout
@@ -81,7 +132,9 @@ function ExpandedCard({ params, setExpanded }) {
       <div>
         <UilTimes onClick={setExpanded} style={{ cursor: "pointer" }} />
         <span>{params.title}</span>
-        <div className="chartContainer">Chart</div>
+        <div className="chartContainer">
+          <Chart series={params.series} type="area" options={data.options} />
+        </div>
         <span>Last 24 hours</span>
       </div>
     </motion.div>
