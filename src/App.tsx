381ff2fdb38f45f185bf0151b9a94b74ff89b37d
Sheonna Harris <svonharris@gmail.com>
2025-08-28 01:00:46 -0400

    Added CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import MainDash from "./components/MainDash/MainDash";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <>
       <div className="app">
         <div className="app-glass">
+          <CssBaseline />
           <Sidebar />
           <MainDash />
         </div>
