f7c60a8af7c5aedffbb5b287691e3d1652ac318b
Sheonna Harris <svonharris@gmail.com>
2025-08-29 16:42:55 -0400

    Fixed react-apexcharts area Chart options prop.

--- src/components/CustomerReview/CustomerReview.tsx
+++ src/components/CustomerReview/CustomerReview.tsx
@@ -1,62 +1,59 @@
 import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 import "./CustomerReview.css";
 
 const CustomerReview = () => {
-  const data = {
-    series: [
-      {
-        name: "Review",
-        data: [10, 50, 30, 90, 40, 120, 100],
+  const options: ApexOptions = {
+    chart: {
+      type: "area",
+      height: "auto",
+    },
+    fill: {
+      colors: ["#fff"],
+      type: "gradient",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+      colors: ["#ff929f"],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
       },
-    ],
-    options: {
-      chart: {
-        type: "area",
-        height: "auto",
-      },
-      fill: {
-        colors: ["#fff"],
-        type: "gradient",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-        colors: ["#ff929f"],
-      },
-      tooltip: {
-        x: {
-          format: "dd/MM/yy HH:mm",
-        },
-      },
-      grid: {
-        show: false,
-      },
-      xaxis: {
-        type: "datetime",
-        categories: [
-          "2022-09-19T00:00:00.000Z",
-          "2022-09-19T01:30:00.000Z",
-          "2022-09-19T02:30:00.000Z",
-          "2022-09-19T03:30:00.000Z",
-          "2022-09-19T04:30:00.000Z",
-          "2022-09-19T05:30:00.000Z",
-          "2022-09-19T06:30:00.000Z",
-        ],
-      },
-      yaxis: {
-        show: false,
-      },
-      toolbar: {
-        show: false,
-      },
+    },
+    grid: {
+      show: false,
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "2022-09-19T00:00:00.000Z",
+        "2022-09-19T01:30:00.000Z",
+        "2022-09-19T02:30:00.000Z",
+        "2022-09-19T03:30:00.000Z",
+        "2022-09-19T04:30:00.000Z",
+        "2022-09-19T05:30:00.000Z",
+        "2022-09-19T06:30:00.000Z",
+      ],
+    },
+    yaxis: {
+      show: false,
     },
   };
 
+  const series = [
+    {
+      name: "Review",
+      data: [10, 50, 30, 90, 40, 120, 100],
+    },
+  ];
+
   return (
     <div className="CustomerReview">
-      <Chart series={data.series} options={data.options} type="area" />
+      <Chart series={series} options={options} type="area" />
     </div>
   );
 };
