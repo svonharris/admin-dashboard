bc949ddf0cae7baf2d5184f480a4eead8c27dfa1
Sheonna Harris <svonharris@gmail.com>
2025-09-02 15:53:22 -0400

    Changed styles={} to sx={}, changed width of Table.

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -56,9 +56,12 @@ function BasicTable() {
       <h3>Recent Orders</h3>
       <TableContainer
         component={Paper}
-        style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+        sx={{
+          boxShadow: "0px 13px 20px 0px #80808029",
+          maxWidth: { xs: "100vw" },
+        }}
       >
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell sx={{ fontWeight: "bold" }}>Product</TableCell>
